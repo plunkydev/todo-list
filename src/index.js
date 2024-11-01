9c5dcee897fe514a91982cd680c425ecb0a2859f
plunkydev <plunkydesign@gmail.com>
2024-11-01 10:34:41 -0500

    import and call to check if there is a data in the localStorange

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { createformTask } from "./modules/formTask"
+import { thereIsData } from "./modules/StorageService";
+(thereIsData())();//
 
 // Añadir el evento al botón después de que se haya añadido al DOM
 const btnFormTask = document.getElementById("btnAddTask");
