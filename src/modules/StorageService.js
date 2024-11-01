1a6ed3e5a1eb30a0f4b0a48359bdaa561165a90b
plunkydev <plunkydesign@gmail.com>
2024-11-01 10:59:57 -0500

    Created function to save projects

--- src/modules/StorageService.js
+++ src/modules/StorageService.js
@@ -2,9 +2,16 @@ import { createTask } from './createTask'
 
 //check if there is data from the app, if not create it by default
 export function thereIsData() {
-    const data = createTask('', '', '', '', false, '')
     if (!localStorage.getItem('generalTaskData')) {
+        const data = createTask('', '', '', '', false, '')
         localStorage.setItem('generalTaskData', JSON.stringify([data]));
     }
 }
 
+export function saveProject(project) {
+    if (!localStorage.getItem(project)) {
+        localStorage.setItem(project, JSON.stringify([]));
+    } else {
+        console.log(`${project} ya existe!`)
+    }
+}
