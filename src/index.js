0d483187d89ec00b66e34181097b1f170f7e013a
plunkydev <plunkydesign@gmail.com>
2024-11-04 19:58:49 -0500

    Added event to addProject button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import { createformTask } from "./modules/formTask"
+import { createformProject } from "./modules/formProject"
 import { thereIsData } from "./modules/StorageService"
 thereIsData();
 
 // Añadir el evento al botón después de que se haya añadido al DOM
 const btnFormTask = document.getElementById("btnAddTask");
 btnFormTask.addEventListener("click", createformTask);
+const btnFormProject = document.getElementById("addProject");
+btnFormProject.addEventListener("click", createformProject);
