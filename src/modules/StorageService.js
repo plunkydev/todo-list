3bcd6cb2ec8213ae5ce7874fc2f0528d133fcbb7
plunkydev <plunkydesign@gmail.com>
2024-11-06 10:27:28 -0500

    Fixed bug that deleted the general project when a new project was added

--- src/modules/StorageService.js
+++ src/modules/StorageService.js
@@ -3,19 +3,18 @@ let data = null;
 // check if there is data from the app, if not create it by default
 export function thereIsData() {
     if (!localStorage.getItem('generalTaskData')) {
-        localStorage.setItem('generalTaskData', JSON.stringify({general: {}}));
+        localStorage.setItem('generalTaskData', JSON.stringify({general: []}));
         data = {};
-    } else {
-        data = JSON.parse(localStorage.getItem('generalTaskData'));
     }
 }
 
 export function saveProject(project) {
     // Verificar si el proyecto ya existe en data
+    data = JSON.parse(localStorage.getItem('generalTaskData'));
     if (data.hasOwnProperty(project)) {
         alert(`${project} Already exists`);
     } else {
-        data[project] = {}; // Agregar el nuevo proyecto a data
+        data[project] = []; // Agregar el nuevo proyecto a data
         localStorage.setItem('generalTaskData', JSON.stringify(data)); // Guardar data en localStorage
     }
 }
