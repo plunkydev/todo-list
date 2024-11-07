5950d5a8ceb016b77a729b0ad1720bc303675c40
plunkydev <plunkydesign@gmail.com>
2024-11-07 14:16:08 -0500

    Import and call to the show projects function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { createformTask } from "./modules/moduleTask"
-import { createformProject } from "./modules/moduleProject"
+import { createformProject, showProjecs } from "./modules/moduleProject"
 import { thereIsData } from "./modules/StorageService"
 thereIsData();
+showProjecs();
 
 // Añadir el evento al botón después de que se haya añadido al DOM
 const btnFormTask = document.getElementById("btnAddTask");
