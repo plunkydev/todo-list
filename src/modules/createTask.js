59981babbdc7d352061b31adf4bdc52b83e0a198
plunkydev <plunkydesign@gmail.com>
2024-11-14 11:20:51 -0500

    1).Creation of the static variable idCount, it was declared to generate an id for each instance concatenated with the received title. 2). Created get method to obtain the idData.

--- src/modules/createTask.js
+++ src/modules/createTask.js
@@ -2,7 +2,9 @@ import { format } from "date-fns";
 
 
 class Task {
+    static idCount = 0;
     constructor(title, description, dueDate, priority, checkList, project) {
+        this._idData = `${++Task.idCount}:${title}`;
         this._title = title === "" ? "Undefined Task" : title;
         this._description = description === "" ? "Undefined Description" : description;
         this._dueDate = dueDate === "" ? format(new Date(), 'yyyy-MM-dd') : dueDate;
@@ -27,6 +29,9 @@ class Task {
     }
     get project() {
         return this._project;
+    }
+    get idData() {
+        return this._idData;
     }
 
     set title(value) {
