832d68e67ce99a073fa2637f79acaee27baff448
plunkydev <plunkydesign@gmail.com>
2024-11-23 20:38:17 -0500

    Added click highlight feature for project elements. Updated event listeners to apply a 'clicked' class for enhanced user feedback. Modified showTasks function calls to include explicit null parameters for consistency.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@ import { createformTask, showTasks } from "./modules/moduleTask"
 import { createformProject, showProjecs } from "./modules/moduleProject"
 import { thereIsData } from "./modules/StorageService"
 import "./styles.css"
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Seleccionamos todos los elementos que deseas que respondan al clic
+    const elements = document.querySelectorAll('.clickable');
+
+    // Iteramos sobre cada elemento y le agregamos un evento de clic
+    elements.forEach(element => {
+        element.addEventListener('click', () => {
+            // Quitamos la clase 'clicked' de todos los elementos
+            elements.forEach(el => el.classList.remove('clicked'));
+
+            // Al hacer clic, le agregamos la clase 'clicked' solo al elemento seleccionado
+            element.classList.add('clicked');
+        });
+    });
+});
+
+
+
 thereIsData();
 showProjecs();
 showTasks()
@@ -12,12 +31,12 @@ btnFormTask.addEventListener("click", createformTask);
 const btnFormProject = document.getElementById("addProject");
 btnFormProject.addEventListener("click", createformProject);
 const btnComplete = document.getElementById("btnComplete");
-btnComplete.addEventListener("click", () => showTasks(null, "complete"));
+btnComplete.addEventListener("click", () => showTasks(null, "complete", null, null, null));
 const btnNotComplete = document.getElementById("btnNotComplete");
-btnNotComplete.addEventListener("click", () => showTasks(null, 'not complete'));
+btnNotComplete.addEventListener("click", () => showTasks(null, 'not complete', null, null, null));
 const btnTodayTasks = document.getElementById("btnTodayTasks");
-btnTodayTasks.addEventListener("click", () => showTasks(null, null, 'today'));
+btnTodayTasks.addEventListener("click", () => showTasks(null, null, 'today', null, null));
 const btnNext7Days = document.getElementById("btnNext7Days");
-btnNext7Days.addEventListener("click", () => showTasks(null, null, null, 'next7Days'));
+btnNext7Days.addEventListener("click", () => showTasks(null, null, null, 'next7Days', null));
 const btnUnfulfilledTask = document.getElementById("btnUnfulfilledTask");
 btnUnfulfilledTask.addEventListener("click", () => showTasks(null, null, null, null, 'unfulfilledTask'));
